9bc1502bb812d6ac8ecd65c318394ee446c8ad1a
mateo <mateo1559@gmail.com>
2025-07-10 00:11:07 +0200

    v7 Implement weapon functionality and update prefabs
    - Added a new Oruzje script to handle projectile instantiation and firing mechanics.
    - Updated Oruzje1 prefab to include the new Oruzje component.
    - Modified Igrac prefab to set speed property to 20 and adjusted child object references.
    - Updated Tamnica scene to reflect changes in prefab properties and ensure correct object references.

---
+++ Assets/Skripte/Oruzje.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oruzje : MonoBehaviour
+{
+    public GameObject projektil;
+    public GameObject cesticeIspucavanje;
+    public Transform mjestoIspucavanja;
+    public float brzinaIspucavanja;
+    float vrijemeIspucavanja;
+
+    private void Update() {
+        Vector2 smjer = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float kut = Mathf.Atan2(smjer.y, smjer.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, kut - 90);
+
+        if (Input.GetMouseButton(0)) {
+            if (Time.time >= vrijemeIspucavanja) {
+                vrijemeIspucavanja = Time.time + brzinaIspucavanja;
+                Instantiate(projektil, mjestoIspucavanja.position, transform.rotation);
+                Instantiate(cesticeIspucavanje, mjestoIspucavanja.position, transform.rotation);
+            }
+        }
+    }
+
+}
