fdff655f419457088bbcf9bcfda027b718efd395
mateo <mateo1559@gmail.com>
2025-07-10 00:10:06 +0200

    v6 Update player movement logic and scene properties
    - Renamed variable 'micanjeBrzina' to 'micanjeUlaz' for clarity in Igrac.cs.
    - Enhanced movement input handling by normalizing the input vector and updating animation states based on movement.
    - Added a new property in the Tamnica scene to set the player's speed to 20.

--- Assets/Skripte/Igrac.cs
+++ Assets/Skripte/Igrac.cs
@@ -7,7 +7,7 @@ public class Igrac : MonoBehaviour {
     public float brzina;
     Rigidbody2D rb;
     Animator anim;
-    Vector2 micanjeBrzina;
+    Vector2 micanjeUlaz;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
@@ -17,7 +17,21 @@ public class Igrac : MonoBehaviour {
     private void Update() {
         float micanjeX = Input.GetAxisRaw("Horizontal");
         float micanjeY = Input.GetAxisRaw("Vertical");
-        Vector2 micanjeUlaz = new Vector2(micanjeX, micanjeY);
-        Debug.Log(micanjeUlaz);
+        micanjeUlaz = new Vector2(micanjeX, micanjeY);
+        micanjeUlaz.Normalize();
+        //micanjeUlaz = micanjeUlaz.normalized;
+
+        //transform.Translate(micanjeUlaz * brzina * Time.deltaTime);
+        //Debug.Log(micanjeUlaz.magnitude);
+
+        if (micanjeUlaz.magnitude == 0) {
+            anim.SetBool("aktivirajHodanje", false);
+        } else {
+            anim.SetBool("aktivirajHodanje", true);
+        }
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + micanjeUlaz * brzina * Time.fixedDeltaTime);
     }
 }
